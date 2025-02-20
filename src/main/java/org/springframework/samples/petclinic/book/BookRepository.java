029a051002057f1df060b3027b44eda797ad3494
Travis Foxley <travis.foxley@kuali.co>
2025-02-20 16:28:22 -0700

    add better example for using JpaRepository

--- src/main/java/org/springframework/samples/petclinic/book/BookRepository.java
+++ src/main/java/org/springframework/samples/petclinic/book/BookRepository.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
-	// Using option 1 or 3 at https://springjava.com/spring-data-jpa/how-to-use-limit-query-results/
-	// is recommended to avoid performance issues with large datasets.
+	// Useful JpaRepository examples: https://springjava.com/spring-data-jpa/how-to-use-limit-query-results/
 	@Transactional(readOnly = true)
 	List<Book> findByPetType(String petType);
 }
